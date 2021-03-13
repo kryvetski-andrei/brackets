c5b058d5a767d7fd23729e618285ed42080d6ff7
Andrei <kryvetskistud@gmail.com>
2021-03-13 19:02:21 +0300

    feat: add solutin

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const stack = []
+  const openedBrackets = []
+  const closedBrackets = []
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    openedBrackets.push(bracketsConfig[i][0])
+    closedBrackets.push(bracketsConfig[i][1])
+  }
+  for (let j = 0; j < str.length; j++) {
+    if (closedBrackets.includes(str[j]) && stack.length > 0) {
+      const bracket = openedBrackets[closedBrackets.indexOf(str[j])]
+      if (stack[stack.length - 1] === bracket || stack[stack.length - 1] === str[j]) {
+        stack.pop()
+      } else if (str[j] !== bracket) {
+        return false
+      } else {
+        stack.push(str[j])
+      }
+    } else {
+      stack.push(str[j])
+    }
+  }
+  return stack.length === 0
 }
